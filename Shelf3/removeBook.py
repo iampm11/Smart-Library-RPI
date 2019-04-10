e20080bcaa92ad2bc13cfe37db5a78d9566adf6c
Parth Manaktala <manaktala.parth@gmail.com>
2019-04-11 03:44:59 +0530

    Updated Shelf 3

--- Shelf3/removeBook.py
+++ Shelf3/removeBook.py
@@ -13,6 +13,7 @@ def loadData():
     data=json.loads(r.text)
     return data
 
+
 def findNameFromTag(tag):
     for i in range(0, No_Of_Books):
         if tag in booksData[i]["Tags"]:
@@ -21,34 +22,36 @@ def findNameFromTag(tag):
             return booksData[i]["Name"]
 
 def removeBook(name,tag):
-     url = "https://fv3md359db.execute-api.ap-south-1.amazonaws.com/final/readlocation?find=" + name
-     r = requests.get(url)
-     data=json.loads(r.text)
-     arr = data[0]["Location"]["Shelf " + str(shelf_number)]
-     arr.remove(tag)
-     data[0]["Location"]["Shelf " + str(shelf_number)] = arr
-     updateurl = "https://fv3md359db.execute-api.ap-south-1.amazonaws.com/final/updatebook"
-     newData = {
-         "bookname" : name,
-         "book" : data[0]["Location"]
-     }
-     print(newData)
-     r = requests.post(updateurl, json=newData)
-     print(r)
+    url = "https://fv3md359db.execute-api.ap-south-1.amazonaws.com/final/readlocation?find=" + name
+        r = requests.get(url)
+        data=json.loads(r.text)
+        print(name)
+        print(data)
+        arr = data[0]["Location"]["Shelf " + str(shelf_number)]
+        arr.remove(tag)
+        data[0]["Location"]["Shelf " + str(shelf_number)] = arr
+        updateurl = "https://fv3md359db.execute-api.ap-south-1.amazonaws.com/final/updatebook"
+            newData = {
+                "bookname" : name,
+                    "book" : data[0]["Location"]
+                        }
+                            print(newData)
+                            r = requests.post(updateurl, json=newData)
+                            print(r)
 
 def updateData(tag,regno):
-     old_json = booksData[index]["Issued"]
-     print (index)
-     print (booksData[index])
-     old_json[tag] = regno
-     data={
-         "bookname" : book_to_remove,
-         "updated" : old_json
-     }
-     print(data)
-     url = "https://fv3md359db.execute-api.ap-south-1.amazonaws.com/final/updatebookinrecord"
-     r = requests.post(url, json=data)
-     print(r)
+    if "Issued" not in  booksData[index]:
+        booksData[index]['Issued'] = {}
+            old_json = booksData[index]['Issued']
+            old_json[tag] = regno
+            data={
+                "bookname" : book_to_remove,
+                    "updated" : old_json
+                        }
+                            print(data)
+                            url = "https://fv3md359db.execute-api.ap-south-1.amazonaws.com/final/updatebookinrecord"
+                                r = requests.post(url, json=data)
+                                print(r)
 
 
 while True:
@@ -67,3 +70,4 @@ while True:
     else:
         print("Thank you for adding the book")
         break
+
